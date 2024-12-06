45b8fe4e68c3022ba9368124f1a372b5dca9b691
ghegostghaugegeg <yubbjnhkebt>
2024-12-06 18:13:07 +0300

    Первая версия

--- PZ/PZ_5/PZ_5_1.py
+++ PZ/PZ_5/PZ_5_1.py
@@ -1,1 +1,24 @@
 # Составить функцию, которая напечатает сорок любых символов.
+
+def print_forty_characters():
+    """
+    Функция напечатает 40 любых символов.
+    В случае возникновения ошибки выводится сообщение об ошибке.
+    """
+    try:
+        # Генерируем 40 символов
+        characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()'
+
+        # Выбираем 40 случайных символов из строки characters
+        import random
+        selected_characters = random.choices(characters, k=40)
+
+        # Печатаем результат
+        print(''.join(selected_characters))
+
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
+
+# Вызываем функцию
+print_forty_characters()
